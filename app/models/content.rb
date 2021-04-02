7f6238cab81301e200108e04d086c6907031b76c
sun-glass <68639801+sun-glass@users.noreply.github.com>
2021-04-02 16:06:12 +0900

    メイン画面CSS調整

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,14 +1,14 @@
 class Content < ApplicationRecord
   validates :category_id, numericality: { other_than: 1 }
-
-  belongs_to :user
-  has_one_attached :image
-
-  validates :title, presence: true, unless: :was_attached?
+  validates :text, presence: true, unless: :was_attached?
 
   def was_attached?
     self.image.attached?
   end
+
+
+  belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
