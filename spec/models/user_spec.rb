7889219c989a035ec86014c841d36bf19ee83895
sun-glass <68639801+sun-glass@users.noreply.github.com>
2021-04-14 23:53:09 +0900

    ランダムの値生成Gem、Faker導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  describe 'ユーザー新規登録できない場合' do
+    it 'nicknameが空だと登録できないこと' do
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nickname can't be blank"
+    end
+    it 'emailが空だと登録できないこと' do
+      @user.email = ''
+      @user.valid?
+      expect( @user.errors.full_messages).to include "Email can't be blank"
+    end
+  end
 end
