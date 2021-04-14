f58ff204a6d6d1dbd1e249650a1b59b22fe547ae
sun-glass <68639801+sun-glass@users.noreply.github.com>
2021-04-15 00:38:29 +0900

    userモデルvalidates修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true do
-    validates :nickname
-    validates :birth_date
-  end
+    validates :nickname, presence: true, length: { maximum: 15 }
+    validates :birth_date, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,16 +4,37 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
-  describe 'ユーザー新規登録できない場合' do
-    it 'nicknameが空だと登録できないこと' do
-      @user.nickname = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Nickname can't be blank"
+  describe 'ユーザー新規登録' do
+    context '新規登録ができる場合' do
+      it '新規登録ができる' do
+        expect(@user).to be_valid
+      end
+      it 'nicknameが15文字以下であれば登録できる' do
+      end
+      it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+      end
     end
-    it 'emailが空だと登録できないこと' do
-      @user.email = ''
-      @user.valid?
-      expect( @user.errors.full_messages).to include "Email can't be blank"
+    context '新規登録ができない場合' do
+      it 'nicknameが空だと登録できないこと' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Nickname can't be blank"
+      end
+      it 'emailが空だと登録できないこと' do
+        @user.email = ''
+        @user.valid?
+        expect( @user.errors.full_messages).to include "Email can't be blank"
+      end
+      it 'passwordが空では登録できない' do
+      end
+      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+      end
+      it 'nicknameが7文字以上では登録できない' do
+      end
+      it '重複したemailが存在する場合登録できない' do
+      end
+      it 'passwordが5文字以下では登録できない' do
+      end
     end
   end
 end
